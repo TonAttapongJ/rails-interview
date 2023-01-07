14f268616a6f7382c24a0c897a652dd312ce77b3
TonAttapongJ <ton.attapong.j@gmail.com>
2023-01-07 11:54:44 +0700

    config: routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,17 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :admin do
+    resources :products
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :products, only: [:index, :show]
+    end
+  end
+
+  resources :products, only: [:index, :show]
+
+  root "products#index"
 end
